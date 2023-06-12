b17f0e4b25ba4f1e7f664bcebdcc686186968231
Airtribunal <airtribunal@gmail.com>
2023-06-12 16:04:38 +0300

    first commit

---
+++ src/components/Main.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const Main = () => {
+    return (
+        <div className='main-content'>
+            <h2>Интересные факты о ReactJs</h2>
+            <ul>
+                <li>Был создан в 2013 году</li>
+                <li>Был изначально создан Джоном Волки</li>
+                <li>У ReactJs более сотни звездочек на Github</li>
+                <li>Поддерживается компанией Facebook</li>
+                <li>На нем построены сотни тысяч проектов для крупных компаний </li>
+            </ul>
+        </div>
+    );
+}
+
+export default Main;
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Navbar = () => {
+    return (
+        <nav>
+            <div className="logo">
+                <img src="././reactjs-icon.png" alt="" />
+                <h4 className="logo-text">ReactFacts</h4>
+            </div>
+            <h5>Кастомные компоненты</h5>
+        </nav>
+    );
+}
+
+export default Navbar;
